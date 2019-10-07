ec152d1e60050b9d5ff00abf596b80e1f3dfc029
Quoc Nhat Han TRAN <quoc_nhat_han.tran@utt.fr>
2019-10-08 01:19:15 +0200

    Updates

---
+++ precache-manifest.0f4bfb09bbb9c3f103deff57fabd3892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da29a995fc0cf21f4e027b3d2bd04b38",
+    "url": "/islands/index.html"
+  },
+  {
+    "revision": "8e427d3f77b38f08fe6e",
+    "url": "/islands/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5e9e2c386398f2defedf",
+    "url": "/islands/static/css/main.8e07ffc1.chunk.css"
+  },
+  {
+    "revision": "8e427d3f77b38f08fe6e",
+    "url": "/islands/static/js/2.98ceae1a.chunk.js"
+  },
+  {
+    "revision": "5e9e2c386398f2defedf",
+    "url": "/islands/static/js/main.90d7e557.chunk.js"
+  },
+  {
+    "revision": "4547802ac08a2204d5df",
+    "url": "/islands/static/js/runtime-main.16eb6c85.js"
+  }
+]);
